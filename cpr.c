eb47472f58c41fd3b48f546fb7a76e8ab151867d
Kristina Zhou <zhou.kristina@utexas.edu>
2023-04-19 17:54:35 -0500

    changed block andsize  queue depth

--- cpr.c
+++ cpr.c
@@ -8,8 +8,8 @@
 #include <unistd.h>
 #include <liburing.h>
 
-#define QUEUE_DEPTH 8
-#define BLOCK_SZ    4096
+#define QUEUE_DEPTH 4
+#define BLOCK_SZ    65536
 
 const int FILE_MODE = S_IRWXU;
 struct stat sb;
