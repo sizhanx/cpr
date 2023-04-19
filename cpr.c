2ca71d729dca888021a4358de0dcd544ea528dd8
Kristina Zhou <zhou.kristina@utexas.edu>
2023-04-19 11:53:40 -0500

    more than queue depth files

--- cpr.c
+++ cpr.c
@@ -9,7 +9,7 @@
 #include <liburing.h>
 
 #define QUEUE_DEPTH 4
-#define BLOCK_SZ    1024
+#define BLOCK_SZ    4096
 
 const int FILE_MODE = S_IRWXU;
 struct stat sb;
@@ -19,14 +19,63 @@ struct io_uring ring;
 uint32_t num_submitted = 0;
 uint32_t num_completed = 0;
 
+typedef struct file_info file_info;
 
 struct file_info {
-    off_t file_sz;
+    off_t offset;
+    file_info* next;
     uint32_t is_write;
+    uint32_t read_fd;
     uint32_t write_fd;
     uint32_t num_blocks;
     struct iovec iovecs[];      /* Referred by readv/writev */
 };
+
+void *main_buffer;
+uint32_t num_buffers_allocated = 0;
+
+struct linked_list {
+  struct file_info *head;
+  struct file_info *tail;
+};
+
+struct linked_list read_list;
+
+void add_to_list(struct file_info *item) {
+  if (read_list.head == NULL) {
+    read_list.head = item;
+    read_list.tail = item;
+  } else {
+    struct file_info *second_to_last = read_list.tail;
+    second_to_last->next = item;
+    read_list.tail = item;
+  }
+}
+
+struct file_info *remove_from_list() {
+  if (read_list.head == NULL) {
+    return NULL;
+  } else if (read_list.head == read_list.tail) {
+    struct file_info *item = read_list.head;
+    read_list.head = NULL;
+    read_list.tail = NULL;
+    return item;
+  } else {
+    struct file_info *item = read_list.head;
+    read_list.head = item->next;
+    item->next = NULL;
+    return item;
+  }
+}
+
+void *get_buffer() {
+  if (num_buffers_allocated < QUEUE_DEPTH) {
+    void *buf = main_buffer + (BLOCK_SZ * num_buffers_allocated);
+    num_buffers_allocated++;
+    return buf;
+  }
+  return NULL;
+}
 
 char *strcpy_m(const char *src) {
   char *temp = malloc(strlen(src));
@@ -68,44 +117,55 @@ int submit_read_request(char *file_path, size_t size, int dest_fd) {
   int current_block = 0;
   int blocks = (int) file_sz / BLOCK_SZ;
   if (file_sz % BLOCK_SZ) blocks++;
-  struct file_info *fi = malloc(sizeof(*fi) +
-                                        (sizeof(struct iovec) * blocks));
-  char *buff = malloc(file_sz);
-  if (!buff) {
-      fprintf(stderr, "Unable to allocate memory.\n");
-      return 1;
-  }
+  // struct file_info *fi = malloc(sizeof(*fi) +
+  //                                       (sizeof(struct iovec) * blocks));
+  // char *buff = malloc(file_sz);
+  // if (!buff) {
+  //     fprintf(stderr, "Unable to allocate memory.\n");
+  //     return 1;
+  // }
 
   while (bytes_remaining) {
       off_t bytes_to_read = bytes_remaining;
       if (bytes_to_read > BLOCK_SZ)
           bytes_to_read = BLOCK_SZ;
 
-      offset += bytes_to_read;
-      fi->iovecs[current_block].iov_len = bytes_to_read;
-      void *buf;
-      if( posix_memalign(&buf, BLOCK_SZ, BLOCK_SZ)) {
-          perror("posix_memalign");
-          return 1;
-      }
-      fi->iovecs[current_block].iov_base = buf;
 
+      struct file_info *fi = (malloc(sizeof(*fi) + sizeof(struct iovec)));
+
+      fi->offset = offset;
+      fi->is_write = 0;
+      fi->read_fd = file_fd;
+      fi->write_fd = dest_fd;
+      fi->num_blocks = blocks;
+      fi->next = NULL;
+      fi->iovecs[0].iov_len = bytes_to_read;
+
+      void *buf = get_buffer();
+      if (buf == NULL) {
+        add_to_list(fi);
+      } else {
+        fi->iovecs[0].iov_base = buf;
+
+        struct io_uring_sqe *sqe = io_uring_get_sqe(&ring);
+        if (sqe == NULL) {
+          perror("could not allocate sqe");
+        }
+        io_uring_prep_readv(sqe, file_fd, fi->iovecs, 1, fi->offset);
+        io_uring_sqe_set_data(sqe, fi);
+      }
+      num_submitted++;
+      // fi->iovecs[current_block].iov_len = bytes_to_read;
+      // void *buf;
+      // if( posix_memalign(&buf, BLOCK_SZ, BLOCK_SZ)) {
+      //     perror("posix_memalign");
+      //     return 1;
+      // }
+      // fi->iovecs[current_block].iov_base = buf;
+      offset += bytes_to_read;
       current_block++;
       bytes_remaining -= bytes_to_read;
   }
-  fi->file_sz = file_sz;
-
-  fi->is_write = 0;
-  fi->write_fd = dest_fd;
-  fi->num_blocks = blocks;
-
-  struct io_uring_sqe *sqe = io_uring_get_sqe(&ring);
-    /* Setup a readv operation */
-  io_uring_prep_readv(sqe, file_fd, fi->iovecs, blocks, 0);
-  /* Set user data */
-  io_uring_sqe_set_data(sqe, fi);
-  /* Finally, submit the request */
-  // io_uring_submit(&ring);
 
   return 0;
 }
@@ -115,7 +175,7 @@ int submit_write_request(int dest_fd, struct file_info *data) {
 
   data->is_write = 1;
 
-  io_uring_prep_writev(sqe, dest_fd, data->iovecs, data->num_blocks, 0);
+  io_uring_prep_writev(sqe, dest_fd, data->iovecs, 1, data->offset);
   io_uring_sqe_set_data(sqe, data);
   io_uring_submit(&ring);
 
@@ -172,7 +232,6 @@ void create_folders(char *src_path, char *dest_path) {
     fallocate(creat_file_fd, 0, 0, sb.st_size);
 
     submit_read_request(src_path, sb.st_size, creat_file_fd);
-    num_submitted++;
 
     free(dest_file_path);
     // close(creat_file_fd);
@@ -191,6 +250,11 @@ int main(int argc, char *argv[]) {
   if (src_abs_path == NULL || dest_abs_path == NULL) {
     goto fail;
   }
+
+  main_buffer = malloc(QUEUE_DEPTH * BLOCK_SZ);
+  memset(main_buffer, 0, QUEUE_DEPTH * BLOCK_SZ);
+  read_list.head = NULL;
+  read_list.tail = NULL;
 
   io_uring_queue_init(QUEUE_DEPTH, &ring, 0);
 
@@ -213,10 +277,22 @@ int main(int argc, char *argv[]) {
 
     if (fi->is_write) {
       num_completed++;
-      close(fi->write_fd);
+      // close(fi->write_fd);
+      struct file_info* next_read = remove_from_list();
+      if (next_read != NULL) {
+        next_read->iovecs[0].iov_base = fi->iovecs[0].iov_base;
+        memset(next_read->iovecs[0].iov_base, 0, BLOCK_SZ);
+
+        struct io_uring_sqe *sqe = io_uring_get_sqe(&ring);
+        io_uring_prep_readv(sqe, next_read->read_fd, next_read->iovecs, 1, next_read->offset);
+        io_uring_sqe_set_data(sqe, next_read);
+
+        io_uring_submit(&ring);
+      }
       free(fi);
     } else {
-      // printf("%s", (char *) (fi->iovecs[0].iov_base));
+      // printf("%.100s\n\n", (char *) (fi->iovecs[0].iov_base));
+      fflush(stdout);
       submit_write_request(fi->write_fd, fi);
     }
     io_uring_cqe_seen(&ring, cqe);
